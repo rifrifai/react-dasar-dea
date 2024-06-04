f57b2382a3b7fa529b804176d44f71a99bd94e30
R. Abdurrohman <rifaiabdurrohman8@gmail.com>
2024-06-04 14:46:06 +0700

    introduction

--- sesi-2/src/App.jsx
+++ sesi-2/src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import Introduction from "./components/Introduction";
+import Profile from "./components/Profile";
 
 function App() {
   return (
     <>
+      <Profile />
       <Introduction name={"Rifai Abdurrohman"} />
       <Introduction name={"Marming Tyas"} />
     </>
---
+++ sesi-2/src/components/Profile.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Profile extends React.Component {
+  render() {
+    return <h2>This is Class</h2>;
+  }
+}
+
+export default Profile;
