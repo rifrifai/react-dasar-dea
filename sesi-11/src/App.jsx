4a751c2b37c676032fd1a3dbffe4a27b40ad911d
R. Abdurrohman <rifaiabdurrohman8@gmail.com>
2024-06-06 12:55:02 +0700

    useEffect

--- sesi-11/src/App.jsx
+++ sesi-11/src/App.jsx
@@ -2,10 +2,12 @@
 import { useState } from "react";
 import "./App.css";
 import Introduction from "./components/Introduction";
+import { useEffect } from "react";
 
 function App() {
   // wife as getter, setWife as setter
   const [wife, setWife] = useState(0);
+  const [wifeName, setWifeName] = useState("");
 
   const ReduceWife = () => {
     if (wife > 0 && wife <= 4) {
@@ -19,8 +21,32 @@ function App() {
     }
   };
 
+  // ini akan di eksekusi jika ada tombol yang dipencet
+  // useEffect(() => {
+  //   console.info("Test");
+  // });
+
+  // ini akan di eksekusi HANYA sekali ketika membuka browser
+  // useEffect(() => {
+  //   alert("UseEffect with { ]");
+  // }, []);
+
+  // ini akan selalu di eksekusi ketika ada value nya
+  useEffect(() => {
+    if (wife == 1) {
+      setWifeName("Arum");
+    } else if (wife == 2) {
+      setWifeName("Marming");
+    } else if (wife == 3) {
+      setWifeName("Tyas");
+    } else {
+      setWifeName("Diah");
+    }
+  }, [wife]);
+
   return (
     <>
+      <h3>My wife is {wifeName}</h3>
       <h3>I have {wife} wife</h3>
       <button onClick={() => AddWife()}>Add wife</button>
       <button onClick={() => ReduceWife()}>Reduce wife</button>
