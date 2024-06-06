b7fd1e798361727436701ccf67681b0b2a7c0706
R. Abdurrohman <rifaiabdurrohman8@gmail.com>
2024-06-06 23:25:30 +0700

    successfull

--- sesi-13/src/App.js
+++ sesi-13/src/App.js
@@ -1,6 +1,32 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import { getMovieList, searchMovie } from "./api";
 
 const App = () => {
+  const [popularMovies, setPopularMovies] = useState([]);
+
+  useEffect(() => {
+    getMovieList().then((result) => {
+      setPopularMovies(result);
+    });
+  }, []);
+
+  const PopularMovieList = () => {
+    return popularMovies.map((movie, i) => {
+      return (
+        <div className="Movie-wrapper" key={i}>
+          <div className="Movie-title">{movie.title}</div>
+          <img
+            className="Movie-image"
+            src={`${process.env.REACT_APP_BASEIMGURL}/${movie.poster_path}`}
+          />
+          <div className="Movie-date">{movie.release_date}</div>
+          <div className="Movie-rate">{movie.vote_average}</div>
+        </div>
+      );
+    });
+  };
+
   const search = (e) => {
     console.info({ e });
   };
@@ -15,12 +41,7 @@ const App = () => {
           onChange={({ target }) => search(target.value)}
         />
         <div className="Movie-container">
-          <div className="Movie-wrapper">
-            <div className="Movie-title">Ini Judul</div>
-            <img className="Movie-image" src="" />
-            <div className="Movie-date">06-06-2024</div>
-            <div className="Movie-rate">9.0</div>
-          </div>
+          <PopularMovieList />
         </div>
       </header>
     </div>
--- sesi-13/src/api.js
+++ sesi-13/src/api.js
@@ -1,8 +1,11 @@
 import axios from "axios";
 
+const apiKey = process.env.REACT_APP_APIKEY;
+const baseUrl = process.env.REACT_APP_BASEURL;
+
 export const getMovieList = async () => {
-  const movie = await axios.get("");
-  return;
+  const movie = await axios.get(`${baseUrl}/movie/popular?api_key=${apiKey}`);
+  return movie.data.results;
 };
 
 export const searchMovie = async (e) => {
